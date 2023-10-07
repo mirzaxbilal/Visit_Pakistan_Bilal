8007261ce64ba7e9a3307c26f14ee7395770de33
mirzaxbilal <87035861+mirzaxbilal@users.noreply.github.com>
2023-10-07 17:59:17 +0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/userRoutes');
+const foodRouter = require('./routes/foodRoutes');
+const restaurantRouter = require('./routes/restaurantRoutes');
 const mongoose = require('mongoose');
 
 const dotenv = require('dotenv').config();
-
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -12,13 +13,13 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/users', userRouter)
-
-
-
 app.get('/', (req, res) => {
     res.send("hello");
 });
+
+app.use('/users', userRouter)
+app.use('/restaurant', restaurantRouter)
+app.use('/food', foodRouter)
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
@@ -28,4 +29,3 @@ mongoose.connect(process.env.MONGO_URL)
     }).catch((error) => {
         console.log(error);
     })
-
