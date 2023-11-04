57d5845e5ebbea4c9fc2b0065d565906446ec993
mirzaxbilal <bilal.baig1973@gmail.com>
2023-11-04 19:43:27 +0500

    commit

--- src/validator/packageValidator.js
+++ src/validator/packageValidator.js
@@ -1,26 +1,26 @@
 const Joi = require('@hapi/joi');
 
 const CreatePackageValidation = Joi.object({
-    title: Joi.string().required().min(10).max(100).required(), // Adjust the min and max lengths as needed
+    title: Joi.string().required().min(10).max(100).required(),
     overview: Joi.string().min(20).required(),
     whatsIncluded: Joi.string().min(10).required(),
     tourItinerary: Joi.string().required(),
-    price: Joi.number().required().positive(), // Assuming the price should be a positive number
-    duration: Joi.number().required().positive(), // Assuming the duration should be a positive number
-    images: Joi.array().items(Joi.string()).required(), // Assuming images are optional or an array of strings
-    locationTags: Joi.array().items(Joi.string()), // Assuming locationTags are optional or an array of strings
+    price: Joi.number().required().positive(),
+    duration: Joi.number().required().positive(),
+    images: Joi.array().items(Joi.string()).required(),
+    locationTags: Joi.array().items(Joi.string()),
 
 });
 
 const UpdatePackageValidation = Joi.object({
-    title: Joi.string().required().min(10).max(100), // Adjust the min and max lengths as needed
+    title: Joi.string().required().min(10).max(100),
     overview: Joi.string().min(20),
     whatsIncluded: Joi.string().min(10),
     tourItinerary: Joi.string(),
-    price: Joi.number().required().positive(), // Assuming the price should be a positive number
-    duration: Joi.number().required().positive(), // Assuming the duration should be a positive number
-    images: Joi.array().items(Joi.string()), // Assuming images are optional or an array of strings
-    locationTags: Joi.array().items(Joi.string()), // Assuming locationTags are optional or an array of strings
+    price: Joi.number().required().positive(),
+    duration: Joi.number().required().positive(),
+    images: Joi.array().items(Joi.string()),
+    locationTags: Joi.array().items(Joi.string()),
     isApproved: Joi.boolean()
 
 });
