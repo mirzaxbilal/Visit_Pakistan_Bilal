626a5ff3fd0e7e062388991278417159d7568283
hamzawalaqadr <mh.hamza01@gmail.com>
2024-01-03 17:04:17 +0500

    Finalized video section

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,16 +3,18 @@ import '../styles/Home.css'
 import Vid from '../assets/videos/home.mp4';
 const home = () => {
     return (
-        <div className="home-container">
+        <>
             <video autoPlay loop muted className="background-video">
                 <source src={Vid} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
-            <div className="content">
-                <h1>Find your pace in Pakistan</h1>
-                <p>Where endless sunshine meets vibrant culture, enriching experiences and limitless adventure.</p>
+            <div className="home-video-container">
+                <div className="content">
+                    <h1>Find your pace in Pakistan</h1>
+                    <p>Where endless sunshine meets vibrant culture, enriching experiences and limitless adventure.</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
